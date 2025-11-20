0d7cf7297bc926db96f213f7166c4a66baef9bad
skyndfly <skyandy2796@yandex.ru>
2025-11-20 22:36:48 +0300

    ссылка на статистику

--- views/layouts/owner-nav.php
+++ views/layouts/owner-nav.php
@@ -1,10 +1,21 @@
+<li class="list-group-item">
+    <a href="/operations-on-manager" class="d-flex align-items-center gap-1">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person"
+             viewBox="0 0 16 16">
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+        </svg>
+        Менеджеры по продажам
+    </a>
+</li>
+<li class="list-group-item d-flex gap-2">
+    <a href="#" class="d-flex align-items-center gap-1">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pie-chart-fill"
+             viewBox="0 0 16 16">
+            <path d="M15.985 8.5H8.207l-5.5 5.5a8 8 0 0 0 13.277-5.5zM2 13.292A8 8 0 0 1 7.5.015v7.778zM8.5.015V7.5h7.485A8 8 0 0 0 8.5.015"/>
+        </svg>
+        Статистика
+    </a>
+    <span class="text-danger ">[в разработке]</span>
+</li>
 
-        <li class="list-group-item">
-            <a href="/operations-on-manager" class="d-flex align-items-center gap-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-                </svg>
-                Менеджеры по продажам
-            </a>
-        </li>
 
